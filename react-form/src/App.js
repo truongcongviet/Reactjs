acedbc75d1fc7cbb12c94ddcb270376fb6243a6e
truongcongviet <tcviet1996@gmail.com>
2021-03-16 11:27:47 +0700

    react-form

---
+++ react-form/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactForm from './react-form';
+import ContentForm from './content-react-form';
+
+function App() {
+  return (
+    <div className="App">
+        <ReactForm></ReactForm>
+    </div>
+  );
+}
+
+export default App;
